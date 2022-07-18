1dd539b80f95d1168a36635bc47fdb02cabf539a
root <root@d05bench.colo.linaro.org>
2022-07-18 11:41:45 +0100

    adding asv-out
    Signed-off-by: root <root@d05bench.colo.linaro.org>

--- asv-conf/benchmarks/benchmarks.py
+++ asv-conf/benchmarks/benchmarks.py
@@ -2,18 +2,18 @@ class TensorflowTimeSuite:
     timeout = 600
     def setup(self):
         import subprocess
-        subprocess.run(['sudo','/home/theodore/resmod/mlperf-setup.sh'])
+        subprocess.run(['sudo','/root/asv-test/asv-conf/asv-test/mlperf-setup.sh'])
         pass
     def time_resnet50(self):
         import subprocess
-#         subprocess.run(['sudo','/home/theodore/resmod/resnet50.sh','restart']
-        subprocess.run(['/home/theodore/resmod/run-resnet50.sh'])
+#         subprocess.run(['sudo','/root/asv-test/asv-conf/asv-test/resnet50.sh','restart'])
+        subprocess.run(['/root/asv-test/asv-conf/asv-test/run-resnet50.sh'])
         pass
     def time_mobilenet(self):
         import subprocess
-        subprocess.run(['/home/theodore/resmod/run-mobilenet.sh'])
+        subprocess.run(['/root/asv-test/asv-conf/asv-test/run-mobilenet.sh'])
         pass
     def time_ssd_resnet34(self):
         import subprocess
-        subprocess.run(['/home/theodore/resmod/run-ssd-resnet34'])
+        subprocess.run(['/root/asv-test/asv-conf/asv-test/run-ssd-resnet34.sh'])
         pass
