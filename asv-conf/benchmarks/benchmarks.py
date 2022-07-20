add628962c666068b6c2dfb8b2903d3bece9c5a3
Theodore Grey <theodore.grey@linaro.org>
2022-07-20 11:10:32 +0200

    updating benchamrks
    Signed-off-by: Theodore Grey <theodore.grey@linaro.org>

--- asv-conf/benchmarks/benchmarks.py
+++ asv-conf/benchmarks/benchmarks.py
@@ -1,19 +1,34 @@
-class TensorflowTimeSuite:
+class TensorflowMLPerfSuite:
     timeout = 600
     def setup(self):
         import subprocess
-        subprocess.run(['/root/asv-test/asv-conf/asv-test/mlperf-setup.sh'], shell=True)
+        subprocess.run(['/root/asv-test/asv-conf/asv-test/mlperf-setup.sh'],  shell=True)
         pass
-    def time_resnet50(self):
+    def QPS_resnet50_list(self):
         import subprocess
-#         subprocess.run(['sudo','/root/asv-test/asv-conf/asv-test/resnet50.sh','restart'], shell=True)
-        subprocess.run(['/root/asv-test/asv-conf/asv-test/run-resnet50.sh'], shell=True)
-        pass
-    def time_mobilenet(self):
+        import re
+        import os
+        subprocess.run(['/root/asv-test/asv-conf/asv-test/run-resnet50.sh'], ">>", "results-resnet50.txt", shell=True)
+        for i, line in enumerate(open("results-resnet5.txt")):
+            if 'qps=' in line:
+                result = re.search('qps=(.*),', line)
+        os.remove("results-resnet50.txt")
+        return result
+    def QPS_mobilenet_list(self):
         import subprocess
-        subprocess.run(['/root/asv-test/asv-conf/asv-test/run-mobilenet.sh'], shell=True)
-        pass
-    def time_ssd_resnet34(self):
-        import subprocess
-        subprocess.run(['/root/asv-test/asv-conf/asv-test/run-ssd-resnet34.sh'], shell=True)
-        pass
+        import re
+        import os
+        subprocess.run(['/root/asv-test/asv-conf/asv-test/run-mobilenet.sh'], ">>", "results-mobilenet.txt", shell=True)
+        for i, line in enumerate(open("results-mobilenet.txt")):
+            if 'qps=' in line:
+                result = re.search('qps=(.*),', line)
+        os.remove("results-mobilenet.txt")
+        return result
+#    def QPS_ssd_resnet34_list(self):
+#        import subprocess
+#        subprocess.run(['/root/asv-test/asv-conf/asv-test/run-ssd-resnet34.sh'], ">>", "results-ssd-resnet-34.txt", shell=True)
+#        for i, line in enumerate(open("results-ssd-resnet34.txt")):
+#            if 'qps=' in line:
+#                result = re.search('qps=(.*),', line)
+#        os.remove("results-ssd-resnet34.txt")
+#        return result
