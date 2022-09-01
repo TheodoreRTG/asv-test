5f7da8dcb113d9caf41f6997247d960a747c1883
Theodore Grey <theodore.grey@linaro.org>
2022-09-01 18:30:18 +0200

    updating path locations

--- run-mobilenet.sh
+++ run-mobilenet.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export DATA_DIR=/home/ubuntu/CK-TOOLS/dataset-coco-2017-val
-export MODEL_DIR=/home/ubuntu/models
-cd ~/sources/inference/vision/classification_and_detection
+export DATA_DIR=/mnt/datasets/data/CK-TOOLS/dataset-coco-2017-val
+export MODEL_DIR=/mnt/datasets/data/models
+cd ~/src/inference/vision/classification_and_detection
 ./run_local.sh tf ssd-mobilenet
