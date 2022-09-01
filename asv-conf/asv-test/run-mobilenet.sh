47ac0fe5ac7930f2dab2628a04e6c1935028fb2b
Theodore Grey <theodore.grey@linaro.org>
2022-09-01 19:35:25 +0200

    Delete asv-conf directory

--- asv-conf/asv-test/run-mobilenet.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export DATA_DIR=/mnt/datasets/data/CK-TOOLS/dataset-coco-2017-val
-export MODEL_DIR=/mnt/datasets/data/models
-cd ~/src/inference/vision/classification_and_detection/
-./run_local.sh tf ssd-mobilenet
