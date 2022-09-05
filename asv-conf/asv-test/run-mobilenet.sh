4cacabc33f8aecb921fc50b13e2fd498b154a1c2
Theodore Grey <theodore.grey@linaro.org>
2022-09-05 15:26:57 +0200

    Update run-mobilenet.sh

--- asv-conf/asv-test/run-mobilenet.sh
+++ asv-conf/asv-test/run-mobilenet.sh
@@ -2,5 +2,5 @@
 
 export DATA_DIR=/mnt/datasets/data/CK-TOOLS/dataset-coco-2017-val
 export MODEL_DIR=/mnt/datasets/data/models
-cd ~/src/inference/vision/classification_and_detection/
+cd /root/asv-test/inference/vision/classification_and_detection/
 ./run_local.sh tf ssd-mobilenet
