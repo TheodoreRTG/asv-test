00a14ff4aabeb075e956253eb39df6d3b4c0175d
Theodore Grey <theodore.grey@linaro.org>
2022-09-05 15:27:45 +0200

    Update run-resnet50.sh

--- run-resnet50.sh
+++ run-resnet50.sh
@@ -2,5 +2,5 @@
 
 export DATA_DIR=/mnt/datasets/data/CK-TOOLS/dataset-imagenet-ilsvrc2012-val-min
 export MODEL_DIR=/mnt/datasets/data/models
-cd ~/src/inference/vision/classification_and_detection/
+cd /root/asv-test/inference/vision/classification_and_detection/
 ./run_local.sh tf resnet50
