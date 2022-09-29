7ec575142c9ebcc01ff9dddcdc866ae94cb41c57
Theodore Grey <theodore.grey@linaro.org>
2022-09-29 16:46:50 +0200

    Update functs.py

--- functs.py
+++ functs.py
@@ -23,6 +23,29 @@ import sys
 #####
 import os
 ##########################
+import pathlib
+import matplotlib
+import matplotlib.pyplot as plt
+import io
+import scipy.misc
+from PIL import Image, ImageDraw, ImageFont
+from six.moves.urllib.request import urlopen
+
+        from functs import 
+#####
+
+def load_image_into_numpy_array(path):
+    image = None 
+    if(path.startswith('http')):
+        response = urlopen(path)
+        image_data = response.read()
+        image_data = BytesIO(image_data)
+        image = Image.open(image_data)
+    else:
+        image_data = tf.io.gfile.GFile(path, 'rb').read()
+        image = Image.open(BytesIO(image_data))
+    (im_width, im_height) = image.size
+    return np.array(image.getdata()).reshape((1, im_height, im_width, 3)).astype(np.uint8)
 
 
 def preprocess_image(image):
@@ -90,7 +113,6 @@ Note: All these models were trained on the ImageNet dataset
 
 
 def run_image_bench(self, model, lib, inter_list, intra_list, batch_size):
-        sys.path.append('/root/asv-test')
                 
         if model == "tp":
           os.environ['TF_ENABLE_ONEDNN_OPTS'] = "1"
@@ -320,3 +342,156 @@ def run_image_bench(self, model, lib, inter_list, intra_list, batch_size):
         print("Inference time:", perf)
         return perf
 
+def run_object_detect_bench(self, model, lib, inter_list, intra_list, batch_size):
+
+        if model == "tp":
+            os.environ['TF_ENABLE_ONEDNN_OPTS'] = "1"
+        else:
+            os.environ['TF_ENABLE_ONEDNN_OPTS'] = "0"
+
+        model_display_name = model
+        inter_op_threads = inter_list
+        intra_op_threads = intra_list
+        mb = batch_size
+        benchname = lib
+
+        print("*"*150)
+        print("Model name =", model_display_name, " batch=", mb, " for ", benchname)
+        # Set TF threads
+        tf.config.threading.set_intra_op_parallelism_threads(intra_op_threads)
+        tf.config.threading.set_inter_op_parallelism_threads(inter_op_threads)
+        print("Inter threads = ", tf.config.threading.get_inter_op_parallelism_threads(), "AND Intra threads = ", tf.config.threading.get_intra_op_parallelism_threads())
+        ############
+        tf.get_logger().setLevel('ERROR')
+
+
+        ALL_MODELS = {
+        'CenterNet HourGlass104 Keypoints 512x512' : 'https://tfhub.dev/tensorflow/centernet/hourglass_512x512_kpts/1',
+        'CenterNet HourGlass104 1024x1024' : 'https://tfhub.dev/tensorflow/centernet/hourglass_1024x1024/1',
+        'CenterNet HourGlass104 Keypoints 1024x1024' : 'https://tfhub.dev/tensorflow/centernet/hourglass_1024x1024_kpts/1',
+        'CenterNet Resnet50 V1 FPN 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v1_fpn_512x512/1',
+        'CenterNet Resnet50 V1 FPN Keypoints 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v1_fpn_512x512_kpts/1',
+        'CenterNet Resnet101 V1 FPN 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet101v1_fpn_512x512/1',
+        'CenterNet Resnet50 V2 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v2_512x512/1',
+        'CenterNet Resnet50 V2 Keypoints 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v2_512x512_kpts/1',
+        'EfficientDet D0 512x512' : 'https://tfhub.dev/tensorflow/efficientdet/d0/1',
+        'EfficientDet D1 640x640' : 'https://tfhub.dev/tensorflow/efficientdet/d1/1',
+        'EfficientDet D2 768x768' : 'https://tfhub.dev/tensorflow/efficientdet/d2/1',
+        'EfficientDet D3 896x896' : 'https://tfhub.dev/tensorflow/efficientdet/d3/1',
+        'EfficientDet D4 1024x1024' : 'https://tfhub.dev/tensorflow/efficientdet/d4/1',
+        'EfficientDet D5 1280x1280' : 'https://tfhub.dev/tensorflow/efficientdet/d5/1',
+        'EfficientDet D6 1280x1280' : 'https://tfhub.dev/tensorflow/efficientdet/d6/1',
+        'EfficientDet D7 1536x1536' : 'https://tfhub.dev/tensorflow/efficientdet/d7/1',
+        'SSD MobileNet v2 320x320' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2',
+        'SSD MobileNet V1 FPN 640x640' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v1/fpn_640x640/1',
+        'SSD MobileNet V2 FPNLite 320x320' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/fpnlite_320x320/1',
+        'SSD MobileNet V2 FPNLite 640x640' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/fpnlite_640x640/1',
+        'SSD ResNet50 V1 FPN 640x640 (RetinaNet50)' : 'https://tfhub.dev/tensorflow/retinanet/resnet50_v1_fpn_640x640/1',
+        'SSD ResNet50 V1 FPN 1024x1024 (RetinaNet50)' : 'https://tfhub.dev/tensorflow/retinanet/resnet50_v1_fpn_1024x1024/1',
+        'SSD ResNet101 V1 FPN 640x640 (RetinaNet101)' : 'https://tfhub.dev/tensorflow/retinanet/resnet101_v1_fpn_640x640/1',
+        'SSD ResNet101 V1 FPN 1024x1024 (RetinaNet101)' : 'https://tfhub.dev/tensorflow/retinanet/resnet101_v1_fpn_1024x1024/1',
+        'SSD ResNet152 V1 FPN 640x640 (RetinaNet152)' : 'https://tfhub.dev/tensorflow/retinanet/resnet152_v1_fpn_640x640/1',
+        'SSD ResNet152 V1 FPN 1024x1024 (RetinaNet152)' : 'https://tfhub.dev/tensorflow/retinanet/resnet152_v1_fpn_1024x1024/1',
+        'Faster R-CNN ResNet50 V1 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet50_v1_640x640/1',
+        'Faster R-CNN ResNet50 V1 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet50_v1_1024x1024/1',
+        'Faster R-CNN ResNet50 V1 800x1333' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet50_v1_800x1333/1',
+        'Faster R-CNN ResNet101 V1 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet101_v1_640x640/1',
+        'Faster R-CNN ResNet101 V1 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet101_v1_1024x1024/1',
+        'Faster R-CNN ResNet101 V1 800x1333' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet101_v1_800x1333/1',
+        'Faster R-CNN ResNet152 V1 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet152_v1_640x640/1',
+        'Faster R-CNN ResNet152 V1 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet152_v1_1024x1024/1',
+        'Faster R-CNN ResNet152 V1 800x1333' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet152_v1_800x1333/1',
+        'Faster R-CNN Inception ResNet V2 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_640x640/1',
+        'Faster R-CNN Inception ResNet V2 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_1024x1024/1',
+        'Mask R-CNN Inception ResNet V2 1024x1024' : 'https://tfhub.dev/tensorflow/mask_rcnn/inception_resnet_v2_1024x1024/1'
+        }
+
+
+        IMAGES_FOR_TEST = {
+          'Beach' : 'models/research/object_detection/test_images/image2.jpg',
+          'Dogs' : 'models/research/object_detection/test_images/image1.jpg',
+          # By Heiko Gorski, Source: https://commons.wikimedia.org/wiki/File:Naxos_Taverna.jpg
+          'Naxos Taverna' : 'https://upload.wikimedia.org/wikipedia/commons/6/60/Naxos_Taverna.jpg',
+         # Source: https://commons.wikimedia.org/wiki/File:The_Coleoptera_of_the_British_islands_(Plate_125)_(8592917784).jpg
+          'Beatles' : 'https://upload.wikimedia.org/wikipedia/commons/1/1b/The_Coleoptera_of_the_British_islands_%28Plate_125%29_%288592917784%29.jpg',
+          # By Américo Toledano, Source: https://commons.wikimedia.org/wiki/File:Biblioteca_Maim%C3%B3nides,_Campus_Universitario_de_Rabanales_007.jpg
+          'Phones' : 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Biblioteca_Maim%C3%B3nides%2C_Campus_Universitario_de_Rabanales_007.jpg/1024px-Biblioteca_Maim%C3%B3nides%2C_Campus_Universitario_de_Rabanales_007.jpg',
+          # Source: https://commons.wikimedia.org/wiki/File:The_smaller_British_birds_(8053836633).jpg
+          'Birds' : 'https://upload.wikimedia.org/wikipedia/commons/0/09/The_smaller_British_birds_%288053836633%29.jpg',
+        }
+
+        COCO17_HUMAN_POSE_KEYPOINTS = [(0, 1),
+         (0, 2),
+         (1, 3),
+         (2, 4),
+         (0, 5),
+         (0, 6),
+         (5, 7),
+         (7, 9),
+         (6, 8),
+         (8, 10),
+         (5, 6),
+         (5, 11),
+         (6, 12),
+         (11, 12),
+         (11, 13),
+         (13, 15),
+         (12, 14),
+         (14, 16)]
+
+        from object_detection.utils import label_map_util
+        from object_detection.utils import visualization_utils as viz_utils
+        from object_detection.utils import ops as utils_ops
+
+        PATH_TO_LABELS = './models/research/object_detection/data/mscoco_label_map.pbtxt'
+        category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+
+        model_handle = ALL_MODELS[model_display_name]
+
+        print('Selected model:'+ model_display_name)
+        print('Model Handle at TensorFlow Hub: {}'.format(model_handle))
+
+        print('loading model...')
+        hub_model = hub.load(model_handle)
+        print('model loaded!')
+
+        selected_image = 'Phones' # @param ['Beach', 'Dogs', 'Naxos Taverna', 'Beatles', 'Phones', 'Birds']
+        flip_image_horizontally = False #@param {type:"boolean"}
+        convert_image_to_grayscale = False #@param {type:"boolean"}
+
+        image_path = IMAGES_FOR_TEST[selected_image]
+        image_np = load_image_into_numpy_array(image_path)
+        print(image_np.shape)
+        # Flip horizontally
+        if(flip_image_horizontally):
+          image_np[0] = np.fliplr(image_np[0]).copy()
+
+        # Convert image to grayscale
+        if(convert_image_to_grayscale):
+          image_np[0] = np.tile(
+            np.mean(image_np[0], 2, keepdims=True), (1, 1, 3)).astype(np.uint8)
+
+        #plt.figure(figsize=(24,32))
+        #plt.imshow(image_np[0])
+        #plt.show()
+        input_shape = image_np.shape 
+        print("SHAPE")
+        print(mb, input_shape[1], input_shape[2], input_shape[3])
+        input_shape = (mb, input_shape[1], input_shape[2], input_shape[3])
+        print("SHAPE AFTER")
+        print(mb, input_shape[1], input_shape[2], input_shape[3])
+
+        warmup_input = tf.random.uniform(input_shape, 0, 1.0)
+        
+        inference_times = []
+        tries=5
+        for _ in range(tries):
+            start_time = time.time_ns()
+            results = hub_model(image_np)
+            end_time = time.time_ns()
+            inference_time = np.round((end_time - start_time) / 1e6, 2)
+            inference_times.append(inference_time)
+            print('DONE,DONE', flush=True)
+        perf=np.min(inference_times)
+        print("Inference time:", perf)
+        return perf
