816aaf3a1852da5ddb3b9028afd0e52b8e6d46bf
Theodore Grey <theodore.grey@linaro.org>
2023-02-13 15:27:31 +0100

    Update gen_commit_list.sh
    Updating to account for new tensorflow-version

--- gen_commit_list.sh
+++ gen_commit_list.sh
@@ -4,5 +4,5 @@ tf_nightly_latest=$(curl -Ls -o /dev/null -w %{url_effective} https://snapshots.
 
 commit_hash=$(curl -Ls https://snapshots.linaro.org/ldcg/python/tensorflow-manylinux-nightly/latest/git_commit_hash)
 
-echo $commit_hash $tf_nightly_latest"tensorflow-aarch64/tensorflow_aarch64-2.12.0-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl 
+echo $commit_hash $tf_nightly_latest"tensorflow-aarch64/tensorflow_aarch64-2.13.0-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl 
 " >> commit_list
